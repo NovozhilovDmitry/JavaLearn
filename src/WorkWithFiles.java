bd1717599cc9ce2b29a3da4bbbfb9a8e01f2d7ee
Dmitry <novozhilovdmitry93@gmail.com>
2025-10-19 22:04:08 +0500

    update

--- src/WorkWithFiles.java
+++ src/WorkWithFiles.java
@@ -10,4 +10,14 @@ public class WorkWithFiles {
         path = Paths.get(p);
     }
 
+    public void printDirectories() {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                System.out.println(entry.getFileName());
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении каталога: " + e.getMessage());
+        }
+    }
+
 }
