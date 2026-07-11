59b025230a96a52a25a4e6195475f6923bdabb1b
Dmitry <novozhilovdmitry93@gmail.com>
2026-07-12 02:18:57 +0500

    add new dict and get dict

--- src/main/java/repository/CustomFieldRepository.java
+++ src/main/java/repository/CustomFieldRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import bd.OracleConnect;
 import bd.SqliteConnect;
 import json.customfield.fieldsdiscription.CustomField;
 import json.customfield.fieldsdiscription.Option;
@@ -15,33 +16,31 @@ public class CustomFieldRepository {
     private final Connection conn;
     private static final Logger log = LoggerFactory.getLogger(CustomFieldRepository.class);
     private final HashMap<Integer, String> components = new HashMap<>();
-    public CustomFieldRepository(SqliteConnect connection) {
+    private final HashMap<String, Integer> idComponents = new HashMap<>();
+
+    public CustomFieldRepository(OracleConnect connection) {
         this.conn = connection.getConnection();
+        fillDict(idComponents);
     }
 
     public void inserIntoTableComponents(List<CustomField> fields) {
-//        String sql =
-//                """
-//                MERGE into components c
-//                USING ( select ? id, ? name from dual) t
-//                ON (c.id=t.id)
-//                WHEN MATCHED THEN
-//                    UPDATE SET c.name=t.name
-//                WHEN NOT MATCHED THEN
-//                    INSERT (id, name) values (t.id, t.name)
-//                """;
-
-        String sql = """
-                INSERT INTO components (
-                    id,
-                    name)
-                VALUES (?, ?)""";
+        String sql =
+                """
+                MERGE into components c
+                USING ( select ? id, ? name from dual) t
+                ON (c.id=t.id)
+                WHEN MATCHED THEN
+                    UPDATE SET c.name=t.name
+                WHEN NOT MATCHED THEN
+                    INSERT (id, name) values (t.id, t.name)
+                """;
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             conn.setAutoCommit(false);
             for (CustomField data: fields) {
                 ps.setInt(1, data.getId());
                 ps.setString(2, data.getName());
+                updateDict(idComponents, data.getName(), data.getId());
                 components.put(data.getId(), data.getName());
                 ps.addBatch();
                 List<Option> options = data.getOptions();
@@ -60,12 +59,32 @@ public class CustomFieldRepository {
             conn.commit();
             log.info("Внесены данные в таблицу COMPONENTS");
         } catch (SQLException e) {
-            log.error("Ошибка добавления данных: {}", e.getMessage());
+            log.error("Ошибка добавления данных: {}", e.getMessage(), e);
         }
     }
 
-    public HashMap<Integer, String> getComponentsDict() {
+    private void fillDict(HashMap<String, Integer> dict) {
+        dict.put("Сервер", null);
+        dict.put("Вид тестирования", null);
+        dict.put("Утвердивший ТК техлид", null);
+        dict.put("Релиз, для которого ТК актуализирован", null);
+        dict.put("Роль пользователя", null);
+        dict.put("Функциональность", null);
+        dict.put("АРМ (начальный статус)", null);
+    }
+
+    private void updateDict(HashMap<String, Integer> dict, String name, Integer value) {
+        if (dict.containsKey(name)) {
+            dict.replace(name, value);
+        }
+    }
+
+    public HashMap<Integer, String> getOptionComponentsDict() {
         return this.components;
+    }
+
+    public HashMap<String, Integer> getMainComponentsDict() {
+        return this.idComponents;
     }
 
     }
